67a1df531f647f7b62c73aca6c492ce44396a9f5
Clayton Cyre <clayton_cyre@hotmail.com>
2026-02-07 19:59:44 -0800

    finalized styling decisions with the header section and featured cards.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import './styles/main.scss'
+import './styles/main.scss';
+import clayImg from './images/featured/clay.jpg';
+import spaceImage1 from './images/body-images/space-image1.jpg';
+import spaceImage2 from './images/body-images/space-image2.jpg';
 import { setupCounter } from './counter.js';
+
+document.querySelector('#project1-img').src = spaceImage1; 
+document.querySelector('#project2-img').src = spaceImage2; 
+document.querySelector('.clay-image').src = clayImg;
 
 setupCounter(document.querySelector('#counter'))
 
